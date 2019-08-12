e98d077fdf27572d4cc5f7bf97a7c1d268cf3073
GitHubForDmitry <dmitrykoka@gmail.com>
2019-08-12 22:29:58 +0300

    Updates

--- precache-manifest.4527dcf86e7e5c8839767da1977cdd4d.js
+++ precache-manifest.4527dcf86e7e5c8839767da1977cdd4d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3875d5fbd27850eb664a54135bb15627",
+    "revision": "05ec659768d638765b32c7a90fce85fa",
     "url": "/react_phone-catalog/index.html"
   },
   {
-    "revision": "e63b77a732af5a14b886",
+    "revision": "40e832ddbb1981f1265f",
     "url": "/react_phone-catalog/static/css/main.54d6ccf4.chunk.css"
   },
   {
-    "revision": "ecdb806936061702c828",
-    "url": "/react_phone-catalog/static/js/2.d60b49f6.chunk.js"
+    "revision": "9fc09122f27fcc855732",
+    "url": "/react_phone-catalog/static/js/2.87f92f6e.chunk.js"
   },
   {
-    "revision": "e63b77a732af5a14b886",
-    "url": "/react_phone-catalog/static/js/main.a7fa262e.chunk.js"
+    "revision": "40e832ddbb1981f1265f",
+    "url": "/react_phone-catalog/static/js/main.75aa3941.chunk.js"
   },
   {
     "revision": "9b3e1d68b9cc1dd9a925",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_phone-catalog/precache-manifest.fd8584930f219693d75f37244b0932f9.js"
+  "/react_phone-catalog/precache-manifest.4527dcf86e7e5c8839767da1977cdd4d.js"
 );
 
 self.addEventListener('message', (event) => {
