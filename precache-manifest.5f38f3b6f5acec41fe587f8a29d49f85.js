5849373b5a7e7089e44f882370288393f16a6dd3
GitHubForDmitry <dmitrykoka@gmail.com>
2019-08-12 23:06:56 +0300

    Updates

--- precache-manifest.5f38f3b6f5acec41fe587f8a29d49f85.js
+++ precache-manifest.5f38f3b6f5acec41fe587f8a29d49f85.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ec0be01a55fd3d076ae42a77d5a6a1ba",
+    "revision": "2e8d506926e2169c4d2db626fd2f9d39",
     "url": "/react_phone-catalog/index.html"
   },
   {
-    "revision": "123df02bc1e17d9791c2",
+    "revision": "1e5f728947189a98ecc4",
     "url": "/react_phone-catalog/static/css/main.54d6ccf4.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_phone-catalog/static/js/2.d60b49f6.chunk.js"
   },
   {
-    "revision": "123df02bc1e17d9791c2",
-    "url": "/react_phone-catalog/static/js/main.8b4decf0.chunk.js"
+    "revision": "1e5f728947189a98ecc4",
+    "url": "/react_phone-catalog/static/js/main.73c6dddf.chunk.js"
   },
   {
     "revision": "9b3e1d68b9cc1dd9a925",
